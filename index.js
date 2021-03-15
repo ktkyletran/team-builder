2a91fa75a1dd1de02617f4d7ce659b2cbcaed9ff
Vanity <ktkyletran@gmail.com>
2021-03-15 15:04:26 -0500

    added init(), fixed html template, removed unneeded code in add employee function section

--- index.js
+++ index.js
@@ -100,8 +100,7 @@ const addManager = () => {
   inquirer
   .prompt(managerPrompts)
   .then((answers) => {
-    const manager = new Manager(answers.name, answers.id, answers.email, answers.office, answers.role)
-    manager.getRole()
+    const manager = new Manager(answers.name, answers.id, answers.email, answers.office)
 
     managersArr.push(manager)
   })
@@ -110,50 +109,38 @@ const addManager = () => {
   })
 }
 
-// Add new engineer
+// Add new employees
 const addEngineer = () => {
   inquirer
   .prompt(engineerPrompts)
   .then((answers) => {
     const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github)
-    engineer.getGithub();
-    engineer.getRole();
 
     engineersArr.push(engineer);
-
-    engineersArr.forEach(newEngineer => generateEngineer(newEngineer));
   })
   .then(() => {
     newMember();
   })
 }
 
-// Add new intern
 const addIntern = () => {
   inquirer
   .prompt(internPrompts)
   .then((answers) => {
     const intern = new Intern(answers.name, answers.id, answers.email, answers.school)
-    intern.getSchool();
-    intern.getRole();
 
     internsArr.push(intern)
-
-    internsArr.forEach(newIntern => generateEngineer(newIntern));
-    console.log(Object.keys({...answers}));
   })
   .then(() => {
     newMember();
   })
 }
 
-// Add new member
 const newMember = () => {
   inquirer
   .prompt(addEmployeePrompts)
   .then((answers) => {
     let roles = answers.role;
-    console.log(answers)
     switch (roles) {
       case 'Manager':
         addManager();
@@ -165,7 +152,7 @@ const newMember = () => {
         addIntern();
         break;
       case "No, I'm finished building my team":
-        renderHTML("teamBuilder.html", generateTeam(answers));
+        renderHTML("teamBuilder.html", generateTeam());
         break;
       default:
         console.log("Error");
@@ -177,18 +164,19 @@ const newMember = () => {
 // Writes HTML file
 const renderHTML = (file, data) => {
   let directory = process.cwd() + "/";
-  console.log('Your HTML has been rendered! (:')
+  console.log('\nYour HTML has been rendered! (:')
   return fs.writeFileSync(path.join(directory, file), data)
 };
 
-const generateTeam = (answers) => {
+// Generates HTML skeleton
+const generateTeam = () => {
   let html = `<!DOCTYPE html>
   <html lang="en">
   <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Team Builder | By Kyle Tran</title>
+    <title>Team Builder || By Kyle Tran</title>
   <!-- Bootstrap -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <!-- Font Awesome -->
@@ -202,7 +190,7 @@ const generateTeam = (answers) => {
       <div class="row">
         <div class="col-md-12" id="title">
           <p>Team<span><i class="fas fa-hammer"></i></span>Builder</p>
-          <h1>By Kyle Tran</h1>
+          <p id="myName">By: Kyle Tran</p>
         </div>
       </div>
     </div>
@@ -223,14 +211,16 @@ const generateTeam = (answers) => {
   `
 
   managersArr.forEach(newManager => {
+    newManager.roles = addEmployeePrompts.role
     let managerHTML = generateManager(newManager)
     html += managerHTML;
-    console.log(managerHTML);
   });
+
   engineersArr.forEach(newEngineer => {
     let engineerHTML = generateEngineer(newEngineer)
     html += engineerHTML;
   });
+
   internsArr.forEach(newIntern => {
     let internHTML = generateIntern(newIntern)
     html += internHTML;
@@ -240,9 +230,9 @@ const generateTeam = (answers) => {
   return html;
 };
 
-// addIntern();
-// addEngineer();
-newMember();
-// renderHTML();
-// addManager();
-// generateTeam();
+// Init
+const init = () => {
+  addManager();
+}
+
+init();
